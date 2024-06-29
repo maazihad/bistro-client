9cc51c285507eba8e5d4e7ce49f50d80da2dc20e
maazihad <maazihadin@gmail.com>
2024-06-29 12:41:02 +0600

    project start

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h2>Bistro app is running...</h2>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>This is from home</div>;
+};
+
+export default Home;
